abe9064f75ea8e724d9e1769bc61d4912199beec
rvn0xsy <payloads@aliyun.com>
2018-03-22 10:32:41 +0800

    add lightbox

---
+++ js/global.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+      $("img").each(function(){
+        $(this).replaceWith("<a href='" + $(this).attr('src')+"' data-lightbox='倾旋'>"+
+          "<img src='" + $(this).attr('src') +"' class='" + $(this).attr('class') +"' alt='" 
+          + $(this).attr('alt') + "' title='" + $(this).attr('title') + "'"
+          + " /></a>");
+      });
+});
