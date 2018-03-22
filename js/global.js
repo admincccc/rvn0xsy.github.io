a9b3c38c308d97df0a20f10262a2a2cbba142bf0
rvn0xsy <payloads@aliyun.com>
2018-03-22 14:53:43 +0800

    fix img

--- js/global.js
+++ js/global.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-      $("img").each(function(){
+      $("article img").each(function(){
         $(this).replaceWith("<a href='" + $(this).attr('src')+"' data-lightbox='倾旋'>"+
           "<img src='" + $(this).attr('src') +"' class='" + $(this).attr('class') +"' alt='" 
           + $(this).attr('alt') + "' title='" + $(this).attr('title') + "'"
